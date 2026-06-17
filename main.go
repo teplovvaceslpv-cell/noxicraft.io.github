21e58aebaf8b565da6ff8eb50c29da2243aa7758
teplovvaceslpv-cell <teplovvaceslpv@gmail.com>
2026-06-17 21:48:13 +0300

    Add files via upload

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+import "net/http"
+
+func main(){
+	http.HandleFunc("/", getMain)
+	http.HandleFunc("/info", getInfo)
+	http.HandleFunc("/pravila", getPr)
+
+	http.ListenAndServe(":8080", nil)
+}
+
+func getMain(w http.ResponseWriter, r *http.Request){
+	http.ServeFile(w, r, "index.html")
+}
+
+func getInfo(w http.ResponseWriter, r *http.Request){
+	http.ServeFile(w, r, "info.html")
+}
+
+func getPr(w http.ResponseWriter, r *http.Request){
+	http.ServeFile(w, r, "pravila.html")
+}
