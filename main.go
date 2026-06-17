b1f854ef2e0495e275b58f40d6ca7fa0ca0c11e5
teplovvaceslpv-cell <teplovvaceslpv@gmail.com>
2026-06-17 21:53:48 +0300

    Update main.go

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import "net/http"
 func main(){
 	http.HandleFunc("/favicon.ico", getFavicon)
 	http.HandleFunc("/", getMain)
-	http.HandleFunc("/info", getInfo)
 	http.HandleFunc("/pravila", getPr)
 
 	http.ListenAndServe(":8080", nil)
@@ -12,10 +11,6 @@ func main(){
 
 func getMain(w http.ResponseWriter, r *http.Request){
 	http.ServeFile(w, r, "index.html")
-}
-
-func getInfo(w http.ResponseWriter, r *http.Request){
-	http.ServeFile(w, r, "info.html")
 }
 
 func getPr(w http.ResponseWriter, r *http.Request){
